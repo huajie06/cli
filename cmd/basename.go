00a208424b7a09f39b093b9731ec6a5ec64b15c8
Huajie Zhang (Mike) <huajie06@gmail.com>
2020-03-14 20:48:47 -0400

    fix fmt.Fprintln

--- cmd/basename.go
+++ cmd/basename.go
@@ -8,17 +8,17 @@ import (
 
 func Basename(s []string) {
 	if len(s) > 1 {
-		fmt.Fprintf(os.Stderr, "too many inputs, please only use 1 input")
+		fmt.Fprintln(os.Stderr, "too many inputs, please only use 1 input")
 		return
 	}
 
 	if len(s) == 0 {
-		fmt.Fprintf(os.Stderr, "Please enter a basename")
+		fmt.Fprintln(os.Stderr, "Please enter a basename")
 		return
 	}
 
 	fp := s[0]
 
 	parts := strings.Split(fp, "/")
-	fmt.Fprintf(os.Stdout, parts[len(parts)-1])
+	fmt.Fprintln(os.Stdout, parts[len(parts)-1])
 }
